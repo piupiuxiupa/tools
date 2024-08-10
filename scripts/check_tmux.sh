23ef5e8038332168c6bab2c131c87af1badb1c00
lush <lush@example.com>
2024-08-10 20:40:09 +0800

    add two scripts of lazy man...

---
+++ scripts/check_tmux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+:'EOF'
+    This script is using to auto boot tmux when you login shell. 
+    I am lazy, so it boom.
+    If you use tmux and lazy too, you can try.
+
+    mv check_tmux.sh /usr/local/bin/check_tmux
+    echo "/usr/local/bin/check_tmux" >> ~/.bashrc
+
+EOF
+
+set -e
+
+test -n "$TMUX" && { echo "Already in tmux."; exit; }
+
+test $(type -P tmux) && { session=$(tmux ls | awk -F: 'NR==1 {print $1}'); } 2> /dev/null
+
+test -n "$session" && tmux attach -t $session
---
+++ scripts/hexo_deploy.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -e 
+
+:'EOF'
+    Well, this script is too short and easy.
+    But I am lazy, so it born.
+    If you use hexo too, you can try this script.
+    chmod +x hexo_deploy.sh
+    mv hexo_deploy.sh /usr/local/bin/hdeploy
+EOF
+
+# Input your codehub address.
+WORKPWD=""
+cd $WORKPWD
+
+function p {
+    git add .
+    git commit -m "${NOTE:-'Update post'}"
+    git push origin hexo
+}
+
+function d {
+    hexo g && hexo d
+    p
+}
+
+test $# -eq 0 && { d; exit 0; }
+
+
+while test $# -ne 0
+do
+    cur="$1"
+    next="$2"
+    case "$cur" in
+        -d)
+            d
+            echo "Deploy"
+        ;;
+        -m)
+            case "$next" in
+                -*)
+                    NOTE="Update post."
+                    ;;
+                *)
+                    NOTE="$next"
+                    shift 
+                    ;;
+            esac
+            echo "Commit Note: $NOTE"
+        ;;
+        -p)
+            p
+            echo "Push code."
+        ;;
+        *)
+            printf "%s: No such option $cur.\n" ${0##*/} >&2
+            echo "Please try other options." >&2
+            exit 2
+        ;;
+    esac 
+    shift
+done
+
+exit 0
